c2d6a00b3e0159ed755a94410bc830e26f836418
Alex <alexundernazarov@gmail.com>
2022-10-23 20:31:58 +0300

    Add 011end

---
+++ L1/L2_011 массивы_03/Program.cs
@@ -0,0 +1,49 @@
+﻿void Fillarray(int[] collection) // void если метод ничего не возвращает
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        
+        collection[index] = new Random().Next(1, 10); // команда, чтоб запихнуть в массив рандомные числа.
+        index++;
+    }
+}
+
+void printArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создали новый массив с размером 10 элементов
+// array[4] = 4; // принудительно назвать элемент
+Fillarray(array); //void FillArray заполнил массив
+printArray(array); //printArray распечатал массив
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
